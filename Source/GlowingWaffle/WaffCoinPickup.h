fa5b52e9f486004026f2f3b3a9d21627edefb7fa
vladimirSirin <shadowsong_sirin@hotmail.com>
2022-05-12 20:03:14 +0800

    - Adding coin pickup, gain credits when interacted

---
+++ Source/GlowingWaffle/WaffCoinPickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WaffPickUp.h"
+#include "WaffCoinPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOWINGWAFFLE_API AWaffCoinPickup : public AWaffPickUp
+{
+	GENERATED_BODY()
+	
+};
